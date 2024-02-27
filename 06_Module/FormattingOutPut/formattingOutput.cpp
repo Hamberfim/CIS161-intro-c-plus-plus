e04f79c00c402ceb848909e753b9bf24183e7bc7
Hamberfim <adhamlin@gmail.com>
2024-02-27 12:12:51 -0600

    layout and calculation working

---
+++ 06_Module/FormattingOutPut/formattingOutput.cpp
@@ -0,0 +1,70 @@
+/* Name: Formatting Output - compounded interest
+ * Author: Anthony Hamlin
+ * CIS161 - 20447
+ * Description:
+ * Write a program that repeatedly asks for the principal, the interest rate
+ * (Rate), and the number of times (T) the interest is compounded and outputs
+ * the amount in savings.
+ * Use the following formula:
+ * Amount in Savings = Principal * (1 + Rate/T)*T
+ *   need #include < math.h >, SavingsAmount = Principal * pow((1+rate/t), t);
+ *   pow(base, exponent);
+ * Also note that rate is input as a percent, but the formula assumes it is
+ * between 0 and 1, so divide the percent by 100.
+ * IMPORTANT: The purpose of this problem is output formatting. Not following
+ * the formatting shown will not receive full credit. Hard coding the formatting
+ * so that it only works for particular inputs will not receive full credit.
+ * The output need to be in this format:
+ *      Enter principal: 1000
+ *      Enter interest rate (as percent): 4.25
+ *      Enter number of times compounded: 12
+ *
+ *      Interest Rate:           4.25%
+ *      Times Compounded:           12
+ *      Principal:         $   1000.00
+ *      Amount in Savings: $   1043.34
+ */
+#include <cctype>
+#include <iomanip>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main(void) {
+  // variables
+  double principal;
+  double interestRate;
+  int timesCompounded;
+  double inSavings;
+  // bool again = true;
+
+  // user input/data capture
+  cout << "Enter principal: " << endl;
+  cin >> principal;
+  cout << "Enter interest rate (as percent): " << endl;
+  cin >> interestRate;
+  cout << "Enter number of times compounded: " << endl;
+  cin >> timesCompounded;
+
+  // calculation
+  inSavings = principal * pow((1 + (interestRate / 100) / timesCompounded),
+                              timesCompounded);
+  cout << endl;
+  // output/display
+  cout << fixed << setprecision(2) << right;
+
+  cout << setw(20) << left << "Interest Rate: ";
+  cout << setw(9) << right << interestRate << "%" << endl;
+
+  cout << setw(20) << left << "Times Compounded: ";
+  cout << setw(10) << right << timesCompounded << endl;
+
+  cout << setw(20) << left << "Principal:         $";
+  cout << setw(10) << right << principal << endl;
+
+  cout << setw(20) << left << "Amount in Savings: $";
+  cout << setw(10) << right << inSavings << endl;
+
+  return 0;
+}
