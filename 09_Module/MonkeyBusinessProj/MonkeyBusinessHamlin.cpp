f7b903bcdd4a661a27541d43c34ba05d38355333
Hamberfim <adhamlin@gmail.com>
2024-03-30 15:31:47 -0500

    started monkey business app

---
+++ 09_Module/MonkeyBusinessProj/MonkeyBusinessHamlin.cpp
@@ -0,0 +1,66 @@
+/* Name: Monkey Business
+ * Author: Anthony Hamlin
+ * CIS161 - 20447
+ * Description:
+ * A local zoo wants to keep track of how many pounds of food each of its three
+ * monkeys eats each day during a typical week. Write a program that stores this
+ * information in a two-dimensional 3 × 5 array, where each row represents a
+ * different monkey, and each column represents a different day of the week.
+ *
+ *  -----------------------
+ *  |[0][0]||[0][1]|[0][2]|
+ *  -----------------------
+ *  |[1][0]||[1][1]|[1][2]|
+ *  -----------------------
+ *  |[2][0]||[2][1]|[2][2]|
+ *  -----------------------
+ *  |[3][0]||[3][1]|[3][2]|
+ *  -----------------------
+ *  |[4][0]||[4][1]|[4][2]|
+ *  -----------------------
+ *
+ * The program should first have the user input the data for each monkey.
+ * Then, it should create a report that includes the following information:
+ *
+ *      Average amount of food eaten per day by the whole family of monkeys.
+ *      The least amount of food eaten during the week by any one monkey.
+ *      The greatest amount of food eaten during the week by any one monkey.
+ *
+ * Write functions:
+ *      average()
+ *      least()
+ *      greatest()
+ *      main()
+ */
+#include <iomanip>
+#include <iostream>
+#include <limits>
+
+// specificity of namespace declarations
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fixed;
+using std::numeric_limits;
+using std::setprecision;
+
+// functions
+// average amount eaten by all
+double AverageAmount() {}
+// least amount eaten by one
+double LeastAmount() {}
+// greatest amount eaten by one
+double GreatestAmount() {}
+
+int main(void) {
+
+  // prompt user for data on each monkey
+
+  // display
+  // Average amount of food eaten per day by all of monkeys.
+  // least amount of food eaten during the week by any one monkey
+  // greatest amount of food eaten during the week by any one monkey.
+
+  return 0;
+}
